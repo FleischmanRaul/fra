89e070ac12ac7f2299cba7a54d035e7433e8b685
FRA <fleischman.raul@gmail.com>
2020-12-29 15:41:04 +0200

    cv added

--- src/Data/Author.elm
+++ src/Data/Author.elm
@@ -21,6 +21,10 @@ all =
       , avatar = Pages.images.author.dillon
       , bio = "Elm developer and educator. Founder of Incremental Elm Consulting."
       }
+    , { name = "Fleischman Raul Andor"
+      , avatar = Pages.images.author.raul
+      , bio = "Software developer and Elm enthusiast."
+      }
     ]
 
 
